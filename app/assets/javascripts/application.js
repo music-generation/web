c1f94f65531cc261f39a77b7a0cdf6f890ca9c06
egeerdogan <ege@egeerdogan.com>
2019-07-24 10:15:10 +0300

    soundfont and midiplayer scripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+  // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
@@ -14,3 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+//= require midiplayer.min
+//= require soundfont-player.min
+
