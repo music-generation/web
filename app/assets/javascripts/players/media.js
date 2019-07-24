057c925a3edad8f6411113d385dc96316b43a1a3
egeerdogan <ege@egeerdogan.com>
2019-07-24 11:04:25 +0300

    initial midi playing

---
+++ app/assets/javascripts/players/media.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  let players = Array.from(document.getElementsByClassName('player'))
+
+  players.forEach((player) => {
+    player.addEventListener('click', function() {
+      let file = fetchMidiFile('mario.mid')
+      if (file) {
+        console.log('Received file')
+        console.log(file)
+      } else {
+        console.log('Did not receive file.')
+      }
+    })
+  })
+})
+
+function fetchMidiFile(fileName) {
+  const http = new XMLHttpRequest()
+  const url = "https://s3.us-south.cloud-object-storage.appdomain.cloud/music-gen-dev/" + fileName
+  http.open('GET', url) 
+  http.setRequestHeader('origin', 'google.com', false)
+  http.send()
+
+  http.onreadystatechange = (e) => {
+    if (this.readyState == 4 && this.status == 200) {
+      return http.response.body  
+    } 
+  }
+}
+
+// https://s3.us-south.cloud-object-storage.appdomain.cloud/music-gen-dev/<fileName>
